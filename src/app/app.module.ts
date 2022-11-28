41b85d0e2b914f3eaf97a40a9f637c524a4ca601
Hadasss <hadas.gadish@gmail.com>
2022-11-28 15:26:25 -0700

    added boilerplate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TopicComponent } from './components/topic/topic.component';
+import { ItemComponent } from './components/item/item.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DashboardComponent, TopicComponent, ItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
