328cc09748270d92a49f17f6a4bb91c256d5b6af
Hadasss <hadas.gadish@gmail.com>
2022-11-28 15:31:57 -0700

    cleared app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TopicComponent } from './components/topic/topic.component';
 import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, TopicComponent, ItemComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    TopicComponent,
+    ItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
