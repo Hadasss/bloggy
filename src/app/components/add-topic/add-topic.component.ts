92564041429cfbf95c466e88b95859c5ae422993
Hadasss <hadas.gadish@gmail.com>
2022-12-01 15:38:13 -0700

    deleted dashboard component

--- src/app/components/add-topic/add-topic.component.ts
+++ src/app/components/add-topic/add-topic.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Topic } from 'src/app/models/topic.interface';
 
 @Component({
   selector: 'app-add-topic',
   templateUrl: './add-topic.component.html',
-  styleUrls: ['./add-topic.component.css']
+  styleUrls: ['./add-topic.component.css'],
 })
 export class AddTopicComponent implements OnInit {
+  @Output() newTopicEvent = new EventEmitter();
 
-  constructor() { }
+  title!: string;
+  addTopic!: boolean;
+  subscription!: Subscription;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public showAddTopic() {
+    // TODO write logic for showing form to add new topic.
+    this.newTopicEvent.emit();
+    console.log('showAddTopic triggered');
   }
-
 }
--- src/app/components/topic/topic.component.ts
+++ src/app/components/topic/topic.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Topic } from '../../models/topic.interface';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.css']
+  styleUrls: ['./topic.component.css'],
 })
 export class TopicComponent implements OnInit {
+  @Input() title!: String;
 
-  constructor() { }
+  topic!: Topic;
 
-  ngOnInit(): void {
+  addNewTopic: boolean = false;
+
+  public topicsArr: Topic[] = [
+    {
+      title: 'School Stuff',
+    },
+    {
+      title: 'Home Improvement',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addTopic(): void {
+    // set addTopic button's value to true
+    this.addNewTopic = true;
+
+    console.log('addTopic emmited');
+    // TODO call the function to display the add topic form
+    // showAddTopic();
   }
-
 }
